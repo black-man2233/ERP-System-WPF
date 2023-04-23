c764993048147424981748ee29331084dadcf17a
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-04-23 20:02:34 +0200

    About page created

---
+++ ERPSYSTEM-GUI/VIew/Pages/About_Page.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ERPSYSTEM_GUI.VIew.Pages;
+
+public partial class About_Page : UserControl
+{
+    public About_Page()
+    {
+        InitializeComponent();
+    }
+}
